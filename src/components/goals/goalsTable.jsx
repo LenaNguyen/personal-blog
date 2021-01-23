62824e41c3cb5b3c3f8097a227036e55d00c98f6
Lena Nguyen <43686735+LenaNguyen@users.noreply.github.com>
2021-01-23 17:35:07 -0500

    Change to minimalist (#3)
    * WIP
    
    * goals table
    
    * goals cms
    
    * delete goals helper

--- src/components/goals/goalsTable.jsx
+++ src/components/goals/goalsTable.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Table as MuiTable} from '@material-ui/core';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import {Box} from '@material-ui/core';
+import {Box, TableBody, TableCell, TableContainer, TableHead, TableRow, Table as MuiTable} from '@material-ui/core';
 import {goalTypes} from '../../utils/constants';
 import BinaryGoalStatus from './binaryGoalStatus';
 import QualitativeGoalStatus from './qualitativeGoalStatus';
--- src/utils/goalsHelper.js
+++
@@ -1,5 +0,0 @@
-export const getQuantitativeStatus = (total, num_completed) => {}
-
-export const getBinaryStatus = completed => {}
-
-export const getQualitativeStatus = rating => {}
